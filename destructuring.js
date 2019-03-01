92d936c6e357c1dc9507a97f1b112c1273548471
Micaiah Buttars <michael.buttars@gmail.com>
2019-03-01 16:04:20 -0700

    finished destructuring

--- destructuring.js
+++ destructuring.js
@@ -21,7 +21,7 @@ var carDetails = {
   Use object destructuring to save the property values from the object carDetails into new variables. 
 */
 
-//Code Here
+let {color, make, model, year} = carDetails
 
 
 
@@ -34,7 +34,7 @@ var carDetails = {
 */
 
 function greeting( obj ) {
-  //Code Here
+  const {firstName, lastName, title} = obj
   
   // Do not edit the code below.
   return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
@@ -53,7 +53,9 @@ function greeting( obj ) {
   Sum up the values and return the total number.
 */
 
-//Code Here
+function totalPopulation({utah, california, texas, arizona}){
+  return utah + california + texas + arizona
+}
 
 
 
@@ -67,7 +69,11 @@ function greeting( obj ) {
   Push these new variables to an array and return the array. 
 */
 
-//Code Here
+function ingredients({carb, fat, protein}){
+  let arr = [];
+  arr.push(carb, fat, protein)
+  return arr
+}
 
 
 
@@ -85,7 +91,19 @@ function greeting( obj ) {
   Find the smallest number of the three and return that number.
 */
 
-//Code Here
+// function largeNumbers( {first, second, third}){
+//   if(first < second && first < third){
+//     return first
+//   } else if(second < first && second < third){
+//     return second
+//   } else if(third < first && third < second){
+//     return third
+//   }
+//   }
+
+function largeNumbers( {first, second, third}){
+  return Math.min(first, second, third)
+}
 
 
 
@@ -97,6 +115,15 @@ function greeting( obj ) {
   Find the longest array and return that array.
 */
 
-//Code Here
+function numberGroups({a, b, c}){
+  if(a.length > b.length && a.length > c.length){
+    return a;
+  }else if(b.length > a.length && b.length > c.length){
+    return b;
+ } else if(c.length > a.length && b.length){
+   return c
+ }
+}
+
 
 
